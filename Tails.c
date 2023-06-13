3903fef365479492c842e5b7f90a6799c74118c9
s1300156 <136040269+s1300156@users.noreply.github.com>
2023-06-13 09:54:05 +0900

    Dice game:inital revision

---
+++ Tails.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h> 
+
+int main() {
+  int i, random,count =0 ;
+  printf("Rolling dice...\n");
+
+  srand(time(NULL));
+
+  for (i = 1; i <= 2; i++) {
+    random = rand() % 6 + 1;
+    printf("Die %d: %d\n", i, random);
+    count += random;
+  }
+  printf("Total value: %d\n", count);
+
+  return 0;
+}
